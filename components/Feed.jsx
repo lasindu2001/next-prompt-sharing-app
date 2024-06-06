63fdb8bf5194edcb8f15038b34eacd92a9d442cf
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-06 11:35:28 +0530

    fetch all prompts to feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [allPosts, setAllPosts] = useState([]);
+  // Search states
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
-export default Feed
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+    setAllPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={() => {}}
+          required
+          className='search_input peer'
+        />
+      </form>
+      {/* All Prompts */}
+      {searchText ? (
+        <PromptCardList
+          data={allPosts}
+          handleTagClick={() => {}}
+        />
+      ) : (
+        <PromptCardList data={allPosts} handleTagClick={() => {}} />
+      )}
+    </section>
+  );
+};
+
+export default Feed;
